c09543b0388f97019ca4ddcab1381fcebf4dadbc
Anil panthi <apanthi21@gmail.com>
2025-08-30 13:27:19 +0545

    Added some states for quizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import logoImg from '../assets/quiz-logo.png';
+
 export default function Header() {
   return (
-    <Header>
-      <img src={logoImg} alt='Quiz Logo' />
-      <h1>React Quiz</h1>
-    </Header>
+    <header>
+      <img src={logoImg} alt='Quiz logo' />
+      <h1>ReactQuiz</h1>
+    </header>
   );
 }
