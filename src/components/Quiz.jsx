704b62cb3c0bd4c2cb6a5d749b4c3ead70068cd2
Anil panthi <apanthi21@gmail.com>
2025-08-30 13:53:58 +0545

    Adding questions and answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,33 @@
 import { useState } from 'react';
+import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
 
-  return <p>Currently active Question</p>;
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort((a, b) => Math.random() - 0.5);
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className='answer'>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
